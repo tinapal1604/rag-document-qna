e97fde982f533dab34b124b6eccfb29c371633eb
tinapal <tinapal.s16@gmail.com>
2026-06-04 13:10:48 +0530

    streamlit chat UI

---
+++ app/streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import sys
+sys.path.append("src/generation")
+sys.path.append("src/ingestion")
+
+from chain import ask
+
+st.title("RAG Document QnA")
+
+#store chat history in session
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+for msg in st.session_state.history:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+if prompt := st.chat_input("Ask a question about your document..."):
+    with st.chat_message("user"):
+        st.write(prompt)
+    answer = ask(prompt, st.session_state.history)
+    with st.chat_message("assistant"):
+        st.write(answer)
+    st.session_state.history.append({"role":"user", "content":prompt})
+    st.session_state.history.append({"role":"assistant", "content":answer})
--- src/ingestion/loader.py
+++ src/ingestion/loader.py
@@ -1,9 +1,23 @@
 import warnings
 warnings.filterwarnings("ignore")
-
+import os
 from langchain_community.document_loaders import PyPDFLoader
 
-def pdf_loader(file_path: str):
-    loader = PyPDFLoader(file_path=file_path)
-    docs = [doc for doc in loader.lazy_load()]
+def pdf_loader(file_path):
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
     return docs
+
+
+def load_folder(folder_path: str)-> list[str]:
+    all_docs = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            path = os.path.join(folder_path, filename)
+            docs = pdf_loader(path)
+            all_docs.extend(docs)
+    return all_docs
+if __name__ == "__main__":
+    folder = r"C:\MTech\rag_doc_qna\data"
+    docs = load_folder(folder)
+    print(len(docs), "pages loaded")
